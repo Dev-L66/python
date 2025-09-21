bff89f10e5c5c5e250b354fcef7144d2909558ce
Dev-L66 <sabeebashil@yahoo.com>
2025-09-21 11:10:36 +0300

    while loop

--- 4/loops.py
+++ 4/loops.py
@@ -47,3 +47,75 @@ for char in original_str:
    reversed_str =  char + reversed_str 
 
 print(reversed_str)
+
+
+# repetitive char in a str
+word = "helloo"
+
+
+for char in word:
+   if word.count(char) == 1:
+      print(char)
+      break
+
+# factorial using while loop
+num = 5
+factorial = 1
+while num > 0:
+   factorial = factorial * num
+   num = num - 1 
+print(f"Factorila of {n} is {factorial}")
+
+
+
+#  validate inpt
+
+while True:
+ number = int(input("Enter value btw 1-10"))
+ if 1<= number <= 10:
+    print("Thanks")
+    break
+ else:
+   print("Invalid number try again!")
+
+
+   #  prime number checker
+
+number = 3
+isPrime = True
+
+if number > 1:
+   for i in range(2, number):
+      if (number % i) == 0:
+         isPrime = False
+         break
+print(isPrime)
+
+
+
+#  duplicate checker
+
+items = ["apple", "banana", "mango", "apple", "orange"]
+
+unique_items = set()
+for item in items:
+   if item in unique_items:
+      print("Duplicate", item)
+      break
+   unique_items.add(item)
+print(unique_items)
+
+
+
+# exponential backoff
+import time
+
+wait_time = 1
+max_retries = 5
+attempts = 0
+
+while attempts < max_retries:
+   print("Attempts",attempts, "wait time", wait_time)
+   time.sleep(wait_time)
+   wait_time *= 2
+   attempts += 1
